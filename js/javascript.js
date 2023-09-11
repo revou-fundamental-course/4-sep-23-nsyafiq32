69fc75a742d996b249d995cee0baf16cd7b76698
nsyafiq32 <53846768+nsyafiq32@users.noreply.github.com>
2023-09-11 22:17:12 +0700

    2nd Push

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+function luas(){
+    let sisi = document.getElementById("inputField").value;
+
+    let hasil = sisi * sisi;
+
+    console.log(hasil);
+}
+
+function keliling(){
+    let sisi = document.getElementById("inputField").value;
+
+    let hasil = 4 * sisi;
+
+    console.log(hasil);
+}
